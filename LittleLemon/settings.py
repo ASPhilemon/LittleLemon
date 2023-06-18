65c4bdf66c9190d01c79dc1286eeec6416fdae72
philemonariko <philemonariko@gmail.com>
2023-06-18 09:08:54 +0300

    MYSQL connection set up

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'HOST': 'localhost',
+        'USER' : 'root',
+        'PASSWORD': 'password',
     }
 }
 
