cee5b9facf6cef0a3b7e1b1b7aa1b7eb7dc0780f
philemonariko <philemonariko@gmail.com>
2023-06-19 17:55:51 +0300

    Menu API views, routes and serializer created

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from .serializers import MenuSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+#MenuItemView
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+#SingleMenuItemView
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
